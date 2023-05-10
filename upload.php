fc38dd4feedcb94e63966db750c9917c94280ebc
Gaetach21 <tachgaetan@gmail.com>
2023-05-10 06:36:44 +0100

    Add upload page

--- upload.php
+++ upload.php
@@ -4,29 +4,49 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/bootstrap.css" rel ="stylesheet" type="text/css" media="all">
+        <link href="css/style.css" rel ="stylesheet" type="text/css" media="all">
+        <link href="css/form.css" rel ="stylesheet" type="text/css" media="all">
     <title>phpexperience - upload de fichiers</title>
+    <style type="text/css">
+      span {color: red; font-size: 1.2em;}
+    </style>
   </head>
 
-  <body> 
-  <div id="main">
-  	<div class="container-fluid p-5 bg-primary text-white text-center">
-  		<h1>phpexperience</h1>
-  		<p>Toute la force du PHP!</p> 
-  	</div>
-  	<div class="container mt-5">
-  		<h3>Formulaire d'upload de fichiers</h3>
-  		<form action="upload.php" method="post" enctype="multipart/form-data">
-        <h4>Upload Fichier</h4>
-        <label for="fileUpload">Fichier:</label>
-        <input type="file" name="photo" id="fileUpload">
-        <input type="submit" name="submit" value="Upload">
-        <p><strong>Note:</strong> Seuls les formats .jpg, .jpeg, .jpeg, .gif, .png sont autorisés jusqu'à une taille maximale de 5 Mo.</p>
-    </form>
+  <body>
+    <!-- En-tete-->
+    <?php include("includes/header.php")?>
+  
 
-    <?php
+    <!-- Logo-->
+    <?php include("includes/logo.php")?>
+
+
+    <section>
+            <div id="main">
+            <div class="container">
+      <h1>Formulaire d'upload de fichiers</h1>
+      <form action="upload.php" method="post" enctype="multipart/form-data">
+        <p>
+         <label for="photo">Sélectionner un fichier à envoyer :</label><br>
+        <input type="file" name="photo" id="photo"> 
+        </p>
+
+        <p>
+         <input type="submit" name="Envoi" value="Envoyer le fichier"> 
+        </p>
+        
+        <p><strong>Note:</strong> Seuls les formats .jpg, .jpeg, .gif et .png sont autorisés jusqu'à une taille maximale de 5 Mo.</p>
+    </form>
+    </div>
+
+    
+
+
+
+
+     <?php
 // Vérifier si le formulaire a été soumis
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if(isset($_POST['Envoi'])){
     // Vérifie si le fichier a été uploadé sans erreur.
     if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
         $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
@@ -36,31 +56,45 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
         // Vérifie l'extension du fichier
         $ext = pathinfo($filename, PATHINFO_EXTENSION);
-        if(!array_key_exists($ext, $allowed)) die("Erreur : Veuillez sélectionner un format de fichier valide.");
+        if(!array_key_exists($ext, $allowed)) die("<span>Veuillez sélectionner un format de fichier valide!</span>");
 
         // Vérifie la taille du fichier - 5Mo maximum
         $maxsize = 5 * 1024 * 1024;
-        if($filesize > $maxsize) die("Error: La taille du fichier est supérieure à la limite autorisée.");
+        if($filesize > $maxsize) die("<span>La taille du fichier est supérieure à la limite autorisée!</span>");
 
         // Vérifie le type MIME du fichier
         if(in_array($filetype, $allowed)){
             // Vérifie si le fichier existe avant de le télécharger.
-            if(file_exists("upload/" . $_FILES["photo"]["name"])){
-                echo $_FILES["photo"]["name"] . " existe déjà.";
+            if(file_exists("uploads/" . $_FILES["photo"]["name"])){
+                echo '<span>Le fichier '.$_FILES["photo"]["name"] . ' existe déjà!</span>';
             } else{
                 move_uploaded_file($_FILES["photo"]["tmp_name"], "upload/" . $_FILES["photo"]["name"]);
-                echo "Votre fichier a été téléchargé avec succès.";
+                echo "<h1>Votre fichier a été envoyé avec succès.</h1>";
             } 
         } else{
-            echo "Error: Il y a eu un problème de téléchargement de votre fichier. Veuillez réessayer."; 
+            echo "<span>Il y a eu un problème de téléchargement de votre fichier. Veuillez réessayer!</span>"; 
         }
     } else{
-        echo "Error: " . $_FILES["photo"]["error"];
+        echo "<span>Erreur: " . $_FILES["photo"]["error"]."</span>";
     }
 }
 ?>
 
-  	</div> 
-  </div> 
+
+    
+      </div>
+
+      <!-- aside-->
+    <?php include("includes/aside.php")?>
+      
+    </section>
+
+   <!-- Pied de page-->
+   <?php include("includes/footer.php")?>
+   
+   
   </body>
 </html>
+
+
+
