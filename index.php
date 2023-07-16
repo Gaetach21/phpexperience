36b1c1b1236d941f6f8f38fcab3dfa62e1a38e5a
Gaetach21 <tachgaetan@gmail.com>
2023-07-16 23:19:09 +0100

    Add cookie consent banner

--- index.php
+++ index.php
@@ -59,7 +59,58 @@
 
 <div id="main">
 
+<div id="cookiePopup" class="hide">
+    <p>
+        <span style="font-weight: 900">phpexperience</span> utilise des cookies pour vous
+        fournir la meilleure expérience de navigation possible. Pour en savoir plus, consultez 
+        notre <a href="#">Politique de confidentialité.</a>
+    </p>
+    <button id="acceptCookie">Accepter</button>
+    <button id="stopCookie" style="background-color: rgb(128,128,128);">Refuser</button>
+</div>
+    <!-- Script -->
+    <script>
+ let popUp = document.getElementById("cookiePopup");
+//When user clicks the accept button
+document.getElementById("acceptCookie").addEventListener("click", () => {
+  //Create date object
+  let d = new Date();
+  //Increment the current time by 1 minute (cookie will expire after 1 minute)
+  d.setMinutes(2 + d.getMinutes());
+  //Create Cookie withname = myCookieName, value = thisIsMyCookie and expiry time=1 minute
+  document.cookie = "myCookieName=thisIsMyCookie; expires = " + d + ";";
+  //Hide the popup
+  popUp.classList.add("hide");
+  popUp.classList.remove("show");
+});
 
+document.getElementById("stopCookie").addEventListener("click", () => {
+  //Hide the popup
+  popUp.classList.add("hide");
+  popUp.classList.remove("show");
+});
+//Check if cookie is already present
+const checkCookie = () => {
+  //Read the cookie and split on "="
+  let input = document.cookie.split("=");
+  //Check for our cookie
+  if (input[0] == "myCookieName") {
+    //Hide the popup
+    popUp.classList.add("hide");
+    popUp.classList.remove("show");
+  } else {
+    //Show the popup
+    popUp.classList.add("show");
+    popUp.classList.remove("hide");
+  }
+};
+//Check if cookie exists when page loads
+window.onload = () => {
+  setTimeout(() => {
+    checkCookie();
+  }, 2000);
+};
+    </script>
     
  <div id="first-section" class="section">
    <h1>Apprenez à coder avec le langage PHP</h1>
