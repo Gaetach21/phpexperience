dee998cd5e0ab9f3f069fffbd9cfa4d4463342c8
Gaetach21 <tachgaetan@gmail.com>
2023-08-02 06:19:26 +0100

    Update minichat

--- minichat.php
+++ minichat.php
@@ -13,8 +13,12 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel ="stylesheet" type="text/css" media="all">
-    <title>phpexperience | un minichat</title>
+    <link href="css/style.css" rel ="stylesheet" type="text/css" media="all">
+    <link href="css/form.css" rel ="stylesheet" type="text/css" media="all">
+    <link href="css/bootstrap.css" rel ="stylesheet" type="text/css" media="all">
+    <link rel="shortcut icon" type="image/ico" href="images/favicon.ico">
+    <script src="jquery-3.6.0.js"></script>
+    <title>phpexperience | Minichat</title>
   </head>
 
   <body>
@@ -50,41 +54,48 @@
 
       <!-- aside-->
     <?php include("includes/aside.php")?>
-    
-            <div id="main">
+  <div id="main">
 
-  <div class="success">
+<div class="success">
     <h1>Bienvenue <?php echo $_SESSION['name']; ?>!</h1>
-    <p>C'est votre espace utilisateur.</p>
+    <?php
+    if ($_SESSION['name'] == 'gaetan') {
+      echo "<p>C'est votre espace admin.</p>";
+    }
+    else
+    {
+      echo "<p>C'est votre espace utilisateur.</p>";
+    }
+    ?>
     <a href="profil.php">Afficher mon profil</a>
     <a href="logout.php">Déconnexion</a>
   </div>
 
-      <div class="container mt-5">
-        <div style="width: 400px; float: left;">
-      <h1>Un Minichat</h1>
-      <form action="minichat_post.php" method="post">
-<p>
-<label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" class="form-control" /><br/>
-<label for="message">Message</label> : <input type="text" name="message" id="message" class="form-control"/><br/>
-<input type="submit" value="Envoyer" class="form-control bg-primary" />
-</p>
-</form>
-</div>
-
-<div style="width: 500px; margin-left: 450px;">
-<h1>Améliorations du minichat</h1>
-<ul>
-  <li><a href="minichat2.php">Retenir le pseudo</a></li>
-  <li><a href="minichat3.php">Afficher les anciens messages</a></li>
-</ul>
-</div>
-
+  	<div class="container">
+  		<h1>Minichat</h1>
+  		<form action="minichat_post.php" method="post">
+  			<label for="pseudo">Votre pseudo</label><br> 
+  			<input type="text" name="pseudo" id="pseudo" value="<?php
+                if(isset($_COOKIE['pseudo']))
+                echo htmlspecialchars ($_COOKIE['pseudo']);
+                ?>" placeholder="Rentre ton pseudo" maxlength="20" required="required"> <br>
+            <label for="message">Message</label><br> 
+            <textarea name="message" id="message" placeholder="Rentre ton message" maxlength="255" required="required"></textarea><br>
+            <input type="submit" name="Envoyer" value="Envoyer" />
+            <input type="reset" name="reset" value="Réinitialiser" />
+		</form>
+ 	</div> 
 
 <h1>Les derniers messages</h1>
 
 <?php
-// Connexion à la base de données
+// On détermine sur quelle page on se trouve
+if(isset($_GET['page']) && !empty($_GET['page'])){
+    $currentPage = (int) strip_tags($_GET['page']);
+}else{
+    $currentPage = 1;
+}
+// On se connecte à là base de données
 try
 {
 $bdd = new PDO('mysql:host=localhost; dbname=phpexperience; charset=utf8', 'root', '123abc456');
@@ -94,25 +105,73 @@ catch(Exception $e)
 die('Erreur : ' .$e->getMessage());
 }
 
-// Récupération des 10 derniers messages
-$reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 5');
+// On détermine le nombre total d'articles
+$sql = 'SELECT COUNT(*) AS nb_articles FROM minichat';
 
-// Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
-while($donnees = $reponse->fetch())
+// On prépare la requête
+$query = $bdd->prepare($sql);
+
+// On exécute
+$query->execute();
+
+// On récupère le nombre d'articles
+$result = $query->fetch();
+
+$nbArticles = (int) $result['nb_articles'];
+
+// On détermine le nombre d'articles par page
+$parPage = 4;
+
+// On calcule le nombre de pages total
+$pages = ceil($nbArticles / $parPage);
+
+// Calcul du 1er article de la page
+$premier = ($currentPage * $parPage) - $parPage;
+
+$sql = 'SELECT * FROM minichat ORDER BY id DESC LIMIT :premier, :parpage';
+
+// On prépare la requête
+$query = $bdd->prepare($sql);
+
+$query->bindValue(':premier', $premier, PDO::PARAM_INT);
+$query->bindValue(':parpage', $parPage, PDO::PARAM_INT);
+
+// On exécute
+$query->execute();
+
+
+
+//on recupère chaque entrée une à une pour l'afficher
+while ($donnees = $query->fetch())
 {
-echo'<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+    echo'<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
 }
-
-$reponse->closeCursor();
-
+$query->closeCursor();
 ?>
-    </div> 
-      </div>
-      
-      <?php include("includes/realisations.php")?>
-      
 
 
+ 
+
+  	<nav>
+                    <ul class="pagination">
+                        <!-- Lien vers la page précédente (désactivé si on se trouve sur la 1ère page) -->
+                        <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
+                            <a href="minichat.php?page=<?= $currentPage - 1 ?>" class="page-link">Précédente</a>
+                        </li>
+                        <?php for($page = 1; $page <= $pages; $page++): ?>
+                          <!-- Lien vers chacune des pages (activé si on se trouve sur la page correspondante) -->
+                          <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
+                                <a href="minichat.php?page=<?= $page ?>" class="page-link"><?= $page ?></a>
+                            </li>
+                        <?php endfor ?>
+                          <!-- Lien vers la page suivante (désactivé si on se trouve sur la dernière page) -->
+                          <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
+                            <a href="minichat.php?page=<?= $currentPage + 1 ?>" class="page-link">Suivante</a>
+                        </li>
+                    </ul>
+    </nav>
+  </div> 
+ 
     </section>
 
    <!-- Pied de page-->
--- minichat_post.php
+++ minichat_post.php
@@ -1,16 +1,43 @@
+<?php
+  // Initialiser la session
+  session_start();
+  // Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
+  if(!isset($_SESSION["name"])){
+    header("Location: login.php");
+    exit(); 
+  }
+?>
+
 <!DOCTYPE html>
 <html>
 
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel ="stylesheet" type="text/css" media="all">
+    <link href="css/style.css" rel ="stylesheet" type="text/css" media="all">
+    <link href="css/form.css" rel ="stylesheet" type="text/css" media="all">
+    <link href="css/bootstrap.css" rel ="stylesheet" type="text/css" media="all">
+    <link rel="shortcut icon" type="image/ico" href="images/favicon.ico">
+    <script src="jquery-3.6.0.js"></script>
     <title>phpexperience | un minichat</title>
   </head>
 
   <body>
     <!-- Logo-->
-    <?php include("includes/logo.php")?>
+    <section class="banniere" id="banniere">
+<div class="contenu">
+<h1>phpexperience</h1>
+<a href="dashboard.php" class="btn" style="background-color: rgb(128,128,128);">Tableau de bord</a>
+
+<!-- <h1>J'apprends le PHP</h1>
+<p>Aujourd'hui nous sommes le 
+<?php
+// echo date('d/m/Y h:i:s');
+?>
+</p> -->
+
+</div>
+</section>
     <!-- En-tete-->
     <?php include("includes/header.php")?>
   
@@ -24,9 +51,23 @@
     <?php include("includes/aside.php")?>
   <div id="main">
 
+<style type="text/css">
+      .disabled{
+        cursor: default;
+        pointer-events: none;
+        text-decoration: none;
+      }
+      span {color: red; font-size: 1.2em;}
+</style>
+
   	<div class="container mt-5">
 
 <?php
+// Création du Cookie
+ 
+setcookie('pseudo', $_POST['pseudo'], time() + 365*24*3600, null, null, false, true);
+
+
 // Connexion à la base de données
 try
 {
@@ -37,25 +78,27 @@ catch(Exception $e)
 die('Erreur : ' .$e->getMessage());
 }
 
-// On teste si le pseudo et le message existent bien
-if (isset($_POST['pseudo']) AND isset($_POST['message'])) {
+//traitement du formulaire:
+if(isset($_POST['submit'])){
 
-// On teste si le pseudo et le message ne sont pas vides
-if (!$_POST['pseudo'] == "" AND !$_POST['message'] == "") {
+//On les rend inoffensives, les balises HTML que le visiteur a pu entrer.
+//On vérifie si tous les champs sont remplis
+    if(empty($_POST['pseudo']) && empty($_POST['message'])){
+    //Si tous les champs sont vides
+        echo "<span>Les deux champs sont obligatoires!</span><br>";
+    } 
 
+    elseif(empty($_POST['pseudo']) || empty($_POST['message'])){
+    //Si un des champs est vide
+        echo "<span>Un des champs est vide!</span><br>";
+    } 
+  
 // Insertion du message à l'aide d'une requête préparée
 $req = $bdd->prepare('INSERT INTO minichat (pseudo, message) VALUES(?, ?)');
 $req->execute(array($_POST['pseudo'], $_POST['message']));
 
 // Redirection du visiteur vers la page du minichat
 header('Location: minichat.php');
-}
-else
-
-// Si le pseudo et le message sont vides et ont été envoyés
-{
-header('Location: minichat.php'); 
-}
 }
 ?>
   	</div> 
