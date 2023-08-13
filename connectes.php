bbf71014e581070ac514e3b1e229252758e48d75
Gaetach21 <tachgaetan@gmail.com>
2023-08-13 18:39:36 +0100

    Update user number online page

--- connectes.php
+++ connectes.php
@@ -13,7 +13,11 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel ="stylesheet" type="text/css" media="all">
+    <link href="css/style.css" rel ="stylesheet" type="text/css" media="all">
+    <link href="css/form.css" rel ="stylesheet" type="text/css" media="all">
+    <link href="css/bootstrap.css" rel ="stylesheet" type="text/css" media="all">
+    <link rel="shortcut icon" type="image/ico" href="images/favicon.ico">
+    <script src="jquery-3.6.0.js"></script>
     <title>phpexperience | un minichat avec compteur de visiteurs</title>
   </head>
 
@@ -43,6 +47,9 @@
         pointer-events: none;
         text-decoration: none;
       }
+      input[type=text], textarea{
+          width: 100%;
+      }
     </style>
 
 
@@ -55,14 +62,23 @@
 
                 <div class="success">
     <h1>Bienvenue <?php echo $_SESSION['name']; ?>!</h1>
-    <p>C'est votre espace utilisateur.</p>
+    <?php
+    if ($_SESSION['name'] == 'gaetan') {
+      echo "<p>C'est votre espace admin.</p>";
+    }
+    else
+    {
+      echo "<p>C'est votre espace utilisateur.</p>";
+    }
+    ?>
     <a href="profil.php">Afficher mon profil</a>
     <a href="logout.php">Déconnexion</a>
   </div>
 
 
-      <div class="container mt-5">
-        <div style="width: 400px; float: left;">
+      <div class="container">
+        <div class="row">
+        <div class="col-sm-6">
       <h1>Un compteur de visiteurs</h1>
       <?php
       try
@@ -94,9 +110,9 @@
 
       $show_user_nbr = $bdd->query("SELECT * FROM visiteurs");
       $user_nbr = $show_user_nbr -> rowCount();
-      echo '<p>Il y a actuellement '.$user_nbr;?> visiteur<?php if ($user_nbr != 1) {
-        echo "s";
-      }?> en ligne sur mon minichat!</p><?php
+      echo '<p>Il y a actuellement '.$user_nbr;?> visiteur<?php if ($user_nbr == 0 || $user_nbr == 1) {
+        echo "";
+      } else{echo "s";}?> en ligne sur mon minichat!</p><?php
       }
 
       catch(PDOException $e)
@@ -105,34 +121,112 @@
       }
 
       ?>
-      <h1>Un minichat</h1>
+      
+      <div class="container">
+      <h1>Minichat</h1>
       <form action="minichat_post.php" method="post">
-<p>
-<label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" class="form-control" /><br/>
-<label for="message">Message</label> : <input type="text" name="message" id="message" class="form-control"/><br/>
-<input type="submit" value="Envoyer" class="form-control bg-primary" />
-</p>
-</form>
+        <label for="pseudo">Votre pseudo</label><br> 
+        <input type="text" name="pseudo" id="pseudo" value="<?php
+                if(isset($_COOKIE['pseudo']))
+                echo htmlspecialchars ($_COOKIE['pseudo']);
+                ?>" placeholder="Rentre ton pseudo" maxlength="20" required="required"> <br>
+            <label for="message">Message</label><br> 
+            <textarea name="message" id="message" placeholder="Rentre ton message" maxlength="255" required="required"></textarea><br>
+            <input type="submit" name="Envoyer" value="Envoyer" />
+            <input type="reset" name="reset" value="Réinitialiser" />
+    </form>
+  </div> 
 </div>
 
-<div style="width: 500px; margin-left: 450px;">
+<div class="col-sm-6">
 <h1>Les derniers messages</h1>
 
 <?php
-
-// Récupération des 10 derniers messages
-$reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 6');
-
-// Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
-while($donnees = $reponse->fetch())
+// On détermine sur quelle page on se trouve
+if(isset($_GET['page']) && !empty($_GET['page'])){
+    $currentPage = (int) strip_tags($_GET['page']);
+}else{
+    $currentPage = 1;
+}
+// On se connecte à là base de données
+try
 {
-echo'<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+$bdd = new PDO('mysql:host=localhost; dbname=phpexperience; charset=utf8', 'root', '123abc456');
+}
+catch(Exception $e)
+{
+die('Erreur : ' .$e->getMessage());
 }
 
-$reponse->closeCursor();
+// On détermine le nombre total d'articles
+$sql = 'SELECT COUNT(*) AS nb_articles FROM minichat';
 
+// On prépare la requête
+$query = $bdd->prepare($sql);
+
+// On exécute
+$query->execute();
+
+// On récupère le nombre d'articles
+$result = $query->fetch();
+
+$nbArticles = (int) $result['nb_articles'];
+
+// On détermine le nombre d'articles par page
+$parPage = 4;
+
+// On calcule le nombre de pages total
+$pages = ceil($nbArticles / $parPage);
+
+// Calcul du 1er article de la page
+$premier = ($currentPage * $parPage) - $parPage;
+
+$sql = 'SELECT * FROM minichat ORDER BY id DESC LIMIT :premier, :parpage';
+
+// On prépare la requête
+$query = $bdd->prepare($sql);
+
+$query->bindValue(':premier', $premier, PDO::PARAM_INT);
+$query->bindValue(':parpage', $parPage, PDO::PARAM_INT);
+
+// On exécute
+$query->execute();
+
+
+
+//on recupère chaque entrée une à une pour l'afficher
+while ($donnees = $query->fetch())
+{
+    echo'<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+}
+$query->closeCursor();
 ?>
+
+
+ 
+
+    <nav>
+                    <ul class="pagination">
+                        <!-- Lien vers la page précédente (désactivé si on se trouve sur la 1ère page) -->
+                        <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
+                            <a href="connectes.php?page=<?= $currentPage - 1 ?>" class="page-link">Précédente</a>
+                        </li>
+                        <?php for($page = 1; $page <= $pages; $page++): ?>
+                          <!-- Lien vers chacune des pages (activé si on se trouve sur la page correspondante) -->
+                          <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
+                                <a href="connectes.php?page=<?= $page ?>" class="page-link"><?= $page ?></a>
+                            </li>
+                        <?php endfor ?>
+                          <!-- Lien vers la page suivante (désactivé si on se trouve sur la dernière page) -->
+                          <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
+                            <a href="connectes.php?page=<?= $currentPage + 1 ?>" class="page-link">Suivante</a>
+                        </li>
+                    </ul>
+    </nav>
+    </div>
     </div> 
+  </div>
+
       </div>
       
       <?php include("includes/realisations.php")?>
