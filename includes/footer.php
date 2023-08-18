8e4ce0619318394c80ef8a43fb79c8c9fb85c234
Gaetach21 <tachgaetan@gmail.com>
2023-08-18 19:23:23 +0100

    Remove contact link from footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,6 @@
  <footer>
       <div class="copyright">
-          <p>Copyright <?php echo date("Y"); ?> <a href="index.php">phpexperience</a>. Tous droits reservés | 
-          	<a href="contact.php">Nous contacter</a> | <a href="admin/home.php">Admin</a>
+          <p>Copyright <?php echo date("Y"); ?> <a href="index.php">phpexperience</a>. Tous droits reservés | <a href="admin/home.php">Admin</a>
 			 
           </p>
       </div>
